8aeebea26eb65c5e906b93edff47071438e26651
Obed Tzay <Ovejatzaypuac03@gmail.com>
2023-05-30 02:47:58 -0600

    corrigiendo error campo virtual, creandos tabla sin ese campo

--- api/bd/migrations/20230517063437-addOrder.js
+++ api/bd/migrations/20230517063437-addOrder.js
@@ -2,13 +2,38 @@
 
 /** @type {import('sequelize-cli').Migration} */
 
-
-const {ORDER_TABLE, OrderSchema} = require('./../models/order.model');
+const {CUSTOMER_TABLE} = require('./../models/customer.model');
+const {ORDER_TABLE} = require('./../models/order.model');
+const {DataTypes, Sequelize} = require('sequelize');
 
 
 module.exports = {
   async up (queryInterface) {
-    await queryInterface.createTable(ORDER_TABLE, OrderSchema);
+    await queryInterface.createTable(ORDER_TABLE, {
+      id:{
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+      },
+      customerId:{
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        field: 'customer_id',
+        references:{
+          model: CUSTOMER_TABLE,
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL'
+      },
+      createAt:{
+        allowNull: false,
+        type: DataTypes.DATE,
+        field: 'created_at',
+        defaultValue: Sequelize.NOW,
+      }
+    });
   },
 
   async down (queryInterface) {
