dc36ee4c5ac2e801c02b94baaf32f0f345fb76f2
Obed Tzay <Ovejatzaypuac03@gmail.com>
2023-05-30 02:03:06 -0600

    corrigiendo error image category

--- api/bd/migrations/20230516015856-modifyCategory.js
+++ api/bd/migrations/20230516015856-modifyCategory.js
@@ -6,7 +6,11 @@ const {CATEGORY_TABLE, CategorySchema}= require('./../models/category.model');
 
 module.exports = {
   async up (queryInterface) {
-    await queryInterface.addColumn(CATEGORY_TABLE, 'image', CategorySchema.image);
+    const categoryTable = await queryInterface.describeTable(CATEGORY_TABLE);
+    if(!categoryTable.image) {
+      await queryInterface.addColumn(CATEGORY_TABLE, 'image', CategorySchema.image);
+    }
+
   },
 
   async down (queryInterface) {
